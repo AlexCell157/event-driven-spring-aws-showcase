62113f817e9f9deba4a7e943280e09ee1a026ad6
Alexander Sel <alexander.sel@wdr-mediagroup.com>
2026-08-28 10:36:06 +0200

    first commit

---
+++ src/main/java/de/asel/eventdrivenspringawsshowcase/EventDrivenSpringAwsShowcaseApplication.java
@@ -0,0 +1,13 @@
+package de.asel.eventdrivenspringawsshowcase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventDrivenSpringAwsShowcaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EventDrivenSpringAwsShowcaseApplication.class, args);
+    }
+
+}
